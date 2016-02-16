871cfb0d91e6e61f88a15651e1d8dea2ef5177d3
Jonathan Haber <jhaber@hubspot.com>
2016-02-16 11:05:34 -0500

    Set content length

--- src/main/java/com/hubspot/maven/plugins/slimfast/DefaultFileUploader.java
+++ src/main/java/com/hubspot/maven/plugins/slimfast/DefaultFileUploader.java
@@ -1,5 +1,6 @@
 package com.hubspot.maven.plugins.slimfast;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.jets3t.service.S3Service;
@@ -7,6 +8,8 @@ import org.jets3t.service.ServiceException;
 import org.jets3t.service.impl.rest.HttpException;
 import org.jets3t.service.model.S3Object;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -21,12 +24,17 @@ public class DefaultFileUploader implements FileUploader {
   }
 
   @Override
-  public void upload(Configuration config, String file) throws MojoFailureException {
+  public void upload(Configuration config, String file) throws MojoExecutionException, MojoFailureException {
     String s3Key = config.getS3ArtifactRoot() + "/" + file;
     Path localPath = Paths.get(config.getRepositoryPath()).resolve(file);
 
     S3Object s3Object = new S3Object(s3Key);
     s3Object.setDataInputFile(localPath.toFile());
+    try {
+      s3Object.setContentLength(Files.size(localPath));
+    } catch (IOException e) {
+      throw new MojoExecutionException("Error reading file at path: " + localPath, e);
+    }
 
     try {
       s3Service.getObjectDetails(config.getS3Bucket(), s3Key);
