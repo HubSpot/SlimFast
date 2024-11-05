86e0c8ebcd855051b712e8534882738ac84015cc
Jared Stehler <jstehler@hubspot.com>
2024-11-04 20:37:42 -0500

    debug manifest output

--- slimfast-plugin/src/main/java/com/hubspot/maven/plugins/slimfast/JsonHelper.java
+++ slimfast-plugin/src/main/java/com/hubspot/maven/plugins/slimfast/JsonHelper.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
@@ -18,8 +19,12 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonHelper {
+
+  private static final Logger LOG = LoggerFactory.getLogger(JsonHelper.class);
 
   public static void writeArtifactsToJson(File outputFile, S3ArtifactWrapper wrapper)
     throws IOException {
@@ -56,6 +61,13 @@ public class JsonHelper {
     try (Writer writer = newWriter(outputFile)) {
       json.writeJSONString(writer);
       writer.flush();
+    }
+
+    if (LOG.isDebugEnabled()) {
+      StringWriter writer = new StringWriter();
+      json.writeJSONString(writer);
+
+      LOG.debug("Wrote artifacts json: {}", writer.toString());
     }
   }
 
